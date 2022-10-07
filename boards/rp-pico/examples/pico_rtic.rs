02ff61f2ea9c1a10d90c9adbad3127293f3460d9
Mathias <mk@blackbird.online>
2022-10-07 10:25:59 +0200

    Add systick and hal monotonics for reference

--- boards/rp-pico/examples/pico_rtic.rs
+++ boards/rp-pico/examples/pico_rtic.rs
@@ -2,23 +2,32 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use panic_halt as _;
 use defmt_rtt as _;
+use panic_halt as _;
 
 #[rtic::app(device = rp_pico::hal::pac, peripherals = true, dispatchers = [XIP_IRQ, USBCTRL_IRQ, PWM_IRQ_WRAP, ADC_IRQ_FIFO])]
 mod app {
 
     use embedded_hal::digital::v2::OutputPin;
-    use fugit::{MicrosDurationU32, ExtU64};
+    use fugit::{ExtU64, MicrosDurationU32};
     use rp_pico::{
         hal::{self, clocks::init_clocks_and_plls, timer::Alarm, watchdog::Watchdog, Sio},
         XOSC_CRYSTAL_FREQ,
     };
 
     defmt::timestamp!("{:?}", monotonics::now());
-    
-    #[monotonic(binds = TIMER_IRQ_0, default = true)]
-    type MyMono = rp2040_monotonic::Rp2040Monotonic;
+
+    // systick monotonic
+    #[monotonic(binds = SysTick, default = true)]
+    type MyMono = systick_monotonic::Systick<1_000>;
+
+    // rp2040_monotonic
+    // #[monotonic(binds = TIMER_IRQ_0, default = true)]
+    // type MyMono = rp2040_monotonic::Rp2040Monotonic;
+
+    // rp-hal monotonic
+    // #[monotonic(binds = TIMER_IRQ_0, default = true)]
+    // type MyMono = hal::timer::monotonic::Monotonic<hal::timer::Alarm0>;
 
     #[shared]
     struct Shared {}
@@ -55,15 +64,21 @@ mod app {
             &mut resets,
         );
 
-        // let timer = hal::Timer::new(c.device.TIMER, &mut resets);
-        // let mut alarm = timer.alarm_0().unwrap();
-        // let _ = alarm.schedule(SCAN_TIME_US);
-        // alarm.enable_interrupt();
-
         blink::spawn().ok();
         foo::spawn().ok();
 
-        (Shared {}, Local {}, init::Monotonics(rp2040_monotonic::Rp2040Monotonic::new(c.device.TIMER)))
+        // systick monotonic
+        let monotonic = systick_monotonic::Systick::new(c.core.SYST, 125_000_000);
+
+        // rp2040_monotonic
+        // let monotonic = rp2040_monotonic::Rp2040Monotonic::new(c.device.TIMER);
+
+        // rp-hal monotonic
+        // let mut timer = hal::Timer::new(c.device.TIMER, &mut resets);
+        // let alarm = timer.alarm_0().unwrap();
+        // let monotonic = hal::timer::monotonic::Monotonic::new(timer, alarm);
+
+        (Shared {}, Local {}, init::Monotonics(monotonic))
     }
 
     #[idle]
@@ -100,14 +115,11 @@ mod app {
         };
 
         futures::join!(task1, task2);
-
     }
-
 
     #[task]
     fn blink(ctx: blink::Context) {
         defmt::info!("hello from blink");
         blink::spawn_after(500.millis()).ok();
     }
-
 }
